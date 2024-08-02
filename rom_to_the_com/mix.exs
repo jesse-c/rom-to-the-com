008983a9ab765ea878682e930385636ca4c95d4b
Jesse Claven <jesse.claven@me.com>
2024-08-02 23:56:38 +0100

    feat(site): Parse films

--- rom_to_the_com/mix.exs
+++ rom_to_the_com/mix.exs
@@ -56,7 +56,8 @@ defmodule RomToTheCom.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:nimble_csv, "~> 1.1"}
     ]
   end
 
