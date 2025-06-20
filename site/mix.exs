a47eb5c7073a1e169821d491da25312a3b47e030
Jesse Claven <jesse.claven@me.com>
2025-06-20 18:55:51 +0100

    build: Use Railpacks

--- site/mix.exs
+++ site/mix.exs
@@ -5,7 +5,7 @@ defmodule RomToTheCom.MixProject do
     [
       app: :rom_to_the_com,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
