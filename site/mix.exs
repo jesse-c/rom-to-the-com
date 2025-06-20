c12d3e753628c8ea03dacff0310023e2e98e3cba
Jesse Claven <jesse.claven@me.com>
2025-06-20 19:15:48 +0100

    build: Use latest dependencies

--- site/mix.exs
+++ site/mix.exs
@@ -53,7 +53,7 @@ defmodule RomToTheCom.MixProject do
        depth: 1},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
+      {:gettext, "~> 0.26.2"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
