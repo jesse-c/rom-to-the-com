77c38a6089aa704df4a2dd62739216360332d9d4
Jesse Claven <jesse.claven@me.com>
2025-08-25 13:13:23 +0100

    ci: Run tests

--- site/test/rom_to_the_com_web/controllers/page_controller_test.exs
+++ site/test/rom_to_the_com_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RomToTheComWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Rom-to-the-Com"
   end
 end
