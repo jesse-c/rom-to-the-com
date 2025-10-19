51815771af732759ecdb85be92a81db3f65c3e4f
Jesse Claven <jesse.claven@me.com>
2025-10-19 23:52:14 +0100

    test(site): Resolve warning

--- site/config/test.exs
+++ site/config/test.exs
@@ -29,3 +29,6 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+# Disable OpenTelemetry exporter during tests
+config :opentelemetry, traces_exporter: :none
