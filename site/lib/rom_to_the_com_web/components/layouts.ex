0bcf3b132dfdf526e2d8a2bffce87239afb45313
Jesse Claven <jesse.claven@me.com>
2026-05-15 12:06:05 +0100

    refactor: Improve SEO

--- site/lib/rom_to_the_com_web/components/layouts.ex
+++ site/lib/rom_to_the_com_web/components/layouts.ex
@@ -11,4 +11,22 @@ defmodule RomToTheComWeb.Layouts do
   use RomToTheComWeb, :html
 
   embed_templates "layouts/*"
+
+  @description "Find the perfect rom-com by adjusting the romance vs comedy slider. Browse and discover romantic comedy films ranked by their rom-to-com ratio."
+
+  def description, do: @description
+
+  def canonical_url do
+    url_config = RomToTheComWeb.Endpoint.config(:url)
+    "https://#{url_config[:host]}/"
+  end
+
+  def schema_markup do
+    JSON.encode!(%{
+      "@context" => "https://schema.org",
+      "@type" => "WebSite",
+      "name" => "Rom-to-the-Com",
+      "description" => @description
+    })
+  end
 end
